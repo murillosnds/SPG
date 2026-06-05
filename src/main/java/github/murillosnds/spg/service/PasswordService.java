816e28134b8e33e92145870e1307392975bf7f7c
Mubru <murillosergionsousa@outlook.com>
2026-06-05 20:24:40 -0300

    refactor: Controller and Service

--- src/main/java/github/murillosnds/spg/service/PasswordService.java
+++ src/main/java/github/murillosnds/spg/service/PasswordService.java
@@ -1,40 +1,67 @@
 package github.murillosnds.spg.service;
 
-import org.passay.CharacterRule;
 import org.passay.CharacterData;
+import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
 import org.passay.PasswordGenerator;
 import org.springframework.stereotype.Service;
+
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class PasswordService {
 
-    public String generatePassword(int length) {
+    private static final int MIN_LENGTH = 12;
+    private static final int MAX_LENGTH = 256;
+    private static final int COUNT_PER_RULE = 2;
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()_+";
 
-        if (length < 12 || length > 256) {
+    private final PasswordGenerator passwordGenerator;
+
+    public PasswordService() {
+        this(new PasswordGenerator());
+    }
+
+    public PasswordService(PasswordGenerator passwordGenerator) {
+        this.passwordGenerator = passwordGenerator;
+    }
+
+    public String generatePassword(int length) {
+        validateLength(length);
+
+        List<CharacterRule> rules = buildRules();
+        return passwordGenerator.generatePassword(length, rules);
+    }
+
+    private void validateLength(int length) {
+        if (length < MIN_LENGTH || length > MAX_LENGTH) {
             throw new IllegalArgumentException(
-                "The size must be between 12 and 256"
+                "The size must be between " + MIN_LENGTH + " and " + MAX_LENGTH
             );
         }
+    }
 
-        PasswordGenerator gen = new PasswordGenerator();
-
-        CharacterRule lowercase = new CharacterRule(EnglishCharacterData.LowerCase, 2);
-        CharacterRule uppercase = new CharacterRule(EnglishCharacterData.UpperCase, 2);
-        CharacterRule digit = new CharacterRule(EnglishCharacterData.Digit, 2);
+    private List<CharacterRule> buildRules() {
+        CharacterRule lowercase = new CharacterRule(EnglishCharacterData.LowerCase, COUNT_PER_RULE);
+        CharacterRule uppercase = new CharacterRule(EnglishCharacterData.UpperCase, COUNT_PER_RULE);
+        CharacterRule digit = new CharacterRule(EnglishCharacterData.Digit, COUNT_PER_RULE);
 
         CharacterData specialChars = new CharacterData() {
-            public String getErrorCode() { return "ERR_SPECIAL"; }
-            public String getCharacters() { return "!@#$%^&*()_+"; }
+            @Override
+            public String getErrorCode() {
+                return "ERR_SPECIAL";
+            }
+
+            @Override
+            public String getCharacters() {
+                return SPECIAL_CHARACTERS;
+            }
         };
-        CharacterRule special = new CharacterRule(specialChars, 2);
 
-        List<CharacterRule> rules = Arrays.asList(lowercase, uppercase, digit, special);
+        CharacterRule special = new CharacterRule(specialChars, COUNT_PER_RULE);
 
-        return gen.generatePassword(length, rules);
-
-        
+        return Arrays.asList(lowercase, uppercase, digit, special);
     }
 }
+
